6c311b125296d25619cb97d79c16492c8d001482
niccolonardelli <niccolo.nardelli@marconibari.edu.it>
2024-10-24 08:17:23 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,35 @@
 #include <iostream>
 using namespace std;
+int main() 
+{
+    int n, i;
+    float numero, max;
+    do 
+    {
+        cout<<"quanti numeri vuoi generare?" << endl;
+        cin >> n;
+    }
+    while (n<1);
+    for(i=1; i<=n; i++)
+    {
+     cout << "inserisci numero" << endl;
+     cin >> numero;
+     if (i==1)
+     {
+        max = numero;
+     }
+     else
+     {
+        if (numero>max)
+        {
+            max= numero;
+        }
+        
+     }
+     
+    }
+   cout << "il numero maggiore è " << max << endl; 
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
